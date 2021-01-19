c13dd3b073a057c84cf9a102d289c501e517fc0e
Yaswant-Kumar-Singhi <singhiyaswant@gmail.com>
2021-01-19 21:53:19 +0530

    why import react

---
+++ src/JSX.js
@@ -0,0 +1,14 @@
+/*
+Why are we importing React and what is the use of importing React
+-> 
+React using JSX which is not a pure JS and browser understands regular JS.
+So these JSX expressions needs to transpiled into regular js syntax so that
+browser can understand.
+
+If you try to remove react import then you will face an error ---> 
+
+---- REACT MUST BE IN SCOPE WHILLE USING JSX-----
+
+That is why we are importing react.
+*/
+import React from 'react'
